e2ec687ad4725d188b5f7c90486e65f15b3dd024
Augusto César <augusto.acfs@gmail.com>
2017-12-04 16:13:39 -0200

    First commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='twitchchatanalyzer',
+      version='0.1',
+      description='Tool for analyzing twitch chat.',
+      url='',
+      author='Augusto Cesar',
+      author_email='augusto.acfs@gmail.com',
+      license='MIT',
+      packages=['twitchchatanalyzer'],
+      install_requires=[
+          'pyyaml',
+          'peewee'
+      ],
+      zip_safe=False)
